07b775384edd66b99ffb669c3438650d892cb0c8
tarmacjupiter <beheryanthony@gmail.com>
2020-07-13 12:56:23 -0500

    Pretty Logo

--- src/components/elements/Logo.js
+++ src/components/elements/Logo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../../assets/images/toplogo.svg";
+import Logo from "../../assets/images/toplogosquare.svg";
 import { Link } from "react-router-dom";
 
 export default function TopLogo() {
@@ -14,7 +14,13 @@ export default function TopLogo() {
       }}
     >
       <Link to="/">
-        <img src={Logo} alt="TOP Logo" width={200} height={200} />
+        <img
+          src={Logo}
+          alt="TOP Logo"
+          width={200}
+          height={200}
+          style={{ marginTop: 45, marginBottom: 5 }}
+        />
       </Link>
     </div>
   );
